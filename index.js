ebca194bb52a9cb710d31a1c1caaa80ced95016e
denny-svg <pknowledge@gmail.com>
2023-12-12 13:10:04 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    let a = (Math.abs(location - 42));
+    return a;
+    }
+
+function distanceFromHqInFeet(location){
+    let a = (Math.abs(location - 42));
+    return a * 264;
+}
+
+function distanceTravelledInFeet(location1, location2) {
+    let a = (Math.abs(location1 - location2));
+    return a * 264;
+}
+
+function calculatesFarePrice(location1, location2) {
+    let a = (Math.abs(location1 - location2) * 264);
+    if (a <=400){
+        return 0;
+    } else if (a > 400 && a <=2000){
+        return ((0.02) * (a - 400));
+    }else if (a > 2000 && a <= 2500){
+        return 25;
+    }else {
+        return ("cannot travel that far");
+    }
+    
+}
